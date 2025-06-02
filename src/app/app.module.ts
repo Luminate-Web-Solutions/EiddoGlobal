28cefa9771a1b0977d4ed7cb76e8d14aadc1023f
Iamfahad948 <fahadullah948@gmail.com>
2025-06-02 11:00:20 +0530

    add components for Home, About Us, Contact, Services, Rentals, Featured Properties, Navbar, and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { RentalsComponent } from './rentals/rentals.component';
+import { FeaturedPropertiesComponent } from './featured-properties/featured-properties.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    NavbarComponent,
+    FooterComponent,
+    RentalsComponent,
+    FeaturedPropertiesComponent
   ],
   imports: [
     BrowserModule,
